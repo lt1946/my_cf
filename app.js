40030e1ed3022d0411c9c5ff125b3f227c16e4b7
iatbforever <lt1946@163.com>
2012-07-05 01:39:28 +0800

    pring  JSON.stringify(process.env.VCAP_SERVICES

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var app = require('express').createServer();
 app.get('/', function(req, res) {
-    res.send('Hello from Cloud Foundry');
+    res.send('Hello from Cloud Foundry'+JSON.stringify(process.env.VCAP_SERVICES));
 });
 app.listen(process.env.VCAP_APP_PORT || 3000);
