e0549ee2f8aba77bf08d158a4f39f6b14b1e87bb
iatbforever <lt1946@163.com>
2012-07-05 01:54:34 +0800

    update

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var app = require('express').createServer();
 app.get('/', function(req, res) {
-    res.send('Hello from Cloud Foundry'+JSON.stringify(process.env.VCAP_SERVICES));
+    var result= 'Hello from Cloud Foundry'
+        +JSON.stringify(process.env.VCAP_SERVICES)+'\n';
+        +JSON.parse(process.env.VCAP_SERVICES)['mysql-5.1'];
+    res.send(result);
 });
 app.listen(process.env.VCAP_APP_PORT || 3000);
