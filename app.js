87e2efb36aa7ed9bf5179bc9507e13f7bb81ba9f
iatbforever <lt1946@163.com>
2012-07-05 13:18:25 +0800

    test

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ app.get('/', function(req, res) {
     var result= 'Hello from Cloud Foundry<br>'
         +JSON.stringify(process.env.VCAP_SERVICES)+'<br>'
         +"========================<br>"
-        +JSON.stringify(JSON.parse(process.env.VCAP_SERVICES)['mysql-5.1']);
+        +JSON.stringify(JSON.parse(process.env.VCAP_SERVICES)['mysql-5.1']['credentials']);
     res.send(result);
 });
 app.listen(process.env.VCAP_APP_PORT || 3000);
