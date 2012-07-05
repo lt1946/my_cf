3fbe1ef9a88cd907958b8566232e84a67b71a95a
iatbforever <lt1946@163.com>
2012-07-05 15:04:04 +0800

    test

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var app = require('express').createServer();
+var b=process.env.VCAP_SERVICES==null;
 app.get('/', function(req, res) {
-    var result= 'Hello from Cloud Foundry<br>'
-        +JSON.stringify(process.env.VCAP_SERVICES)+
-        '<br>========================<br>'
-        +JSON.stringify(JSON.parse(JSON.stringify(JSON.parse(process.env.VCAP_SERVICES)['mysql-5.1']))[0]['credentials']);
-    res.send(result);
+	var result = 'Hello from Cloud Foundry<br>' +
+       ( b?'':JSON.stringify(process.env.VCAP_SERVICES) +
+        '<br>========================<br>' +
+        JSON.stringify(JSON.parse(JSON.stringify(JSON.parse(process.env.VCAP_SERVICES)['mysql-5.1']))[0]['credentials']));
+	res.send(result);
 });
-app.listen(process.env.VCAP_APP_PORT || 3000);
+app.listen(b?3000:process.env.VCAP_APP_PORT);
