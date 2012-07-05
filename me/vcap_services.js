e65b72564ff48303c434386a9d0e7326fef9f03f
iatbforever <lt1946@163.com>
2012-07-05 13:47:38 +0800

    test

---
+++ me/vcap_services.js
@@ -0,0 +1,45 @@
+var v = {
+	"redis-2.2" : [{
+		"name" : "redis-my_cf",
+		"label" : "redis-2.2",
+		"plan" : "free",
+		"tags" : ["redis", "redis-2.2", "key-value", "nosql"],
+		"credentials" : {
+			"hostname" : "172.30.48.41",
+			"host" : "172.30.48.41",
+			"port" : 5098,
+			"password" : "8cc8c72e-f34a-4a4a-a18f-9440ca5dc2a8",
+			"name" : "2891cee2-d559-411a-84ed-fd7e85dbf336"
+		}
+	}],
+	"mysql-5.1" : [{
+		"name" : "mysql_my_cf",
+		"label" : "mysql-5.1",
+		"plan" : "free",
+		"tags" : ["mysql", "mysql-5.1", "relational"],
+		"credentials" : {
+			"name" : "d24f2da09e51f4ccda13f7301261a0666",
+			"hostname" : "172.30.48.27",
+			"host" : "172.30.48.27",
+			"port" : 3306,
+			"user" : "uc6VtSEPySBY3",
+			"username" : "uc6VtSEPySBY3",
+			"password" : "pTviSIsEYbD8a"
+		}
+	}],
+	"mongodb-1.8" : [{
+		"name" : "mongodb-my_cf",
+		"label" : "mongodb-1.8",
+		"plan" : "free",
+		"tags" : ["mongodb", "mongodb-1.8", "nosql", "document"],
+		"credentials" : {
+			"hostname" : "172.30.48.75",
+			"host" : "172.30.48.75",
+			"port" : 25123,
+			"username" : "353071eb-833b-4d53-908c-3eaeef1088da",
+			"password" : "ba7479df-50d9-419e-82f4-991a139a41ee",
+			"name" : "e132aff2-0922-4ec1-81ae-5fa41c81f17b",
+			"db" : "db"
+		}
+	}]
+}
